2d5a8cb550d618d5e16fa1424beab69084758b11
CrkJohn <Jhonhrge@gmail.com>
2020-11-16 23:27:09 -0500

    Base project created

---
+++ src/main/java/technical/test/massiv/MassivApplication.java
@@ -0,0 +1,13 @@
+package technical.test.massiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/technical/test/massiv/MassivApplicationTests.java
@@ -0,0 +1,13 @@
+package technical.test.massiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
